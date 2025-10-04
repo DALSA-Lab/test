aa4a34ff303de274b3e8bb4213e69938b7089d68
Alan Mansour <57322272+alanmansour@users.noreply.github.com>
2025-10-04 15:30:52 +0200

    Fix

---
+++ site/api-cpp/html/dir_0c963d1cbf18cb8e49a41793502d34d3.js
@@ -0,0 +1,4 @@
+var dir_0c963d1cbf18cb8e49a41793502d34d3 =
+[
+    [ "include", "dir_af0444ea0c7ec8635d6db1da8eb19d03.html", "dir_af0444ea0c7ec8635d6db1da8eb19d03" ]
+];
---
+++ site/api-cpp/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['calculator_0',['Calculator',['../classcpppkg_1_1_calculator.html',1,'cpppkg']]]
+];
